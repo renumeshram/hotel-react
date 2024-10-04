e4e0dcafac017a08d883127cb2f0a978feceb54f
Renu  Meshram <163387515+renumeshram@users.noreply.github.com>
2024-10-05 05:21:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Slider from './components/Slider';
+
+
+function App() {
+  return (
+    <div className="explore">
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
